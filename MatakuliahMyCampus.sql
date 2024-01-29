6373ca50ebd1b999954056d627a19079b40ef18b
Yulia Dwi Cahyani <156802112+DwiCahyaYulia@users.noreply.github.com>
2024-01-29 22:19:37 +0700

    Add files via upload
    [FIX] fix bug in data matakuliah

---
+++ MatakuliahMyCampus.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE matakuliah(
+Kode_MK VARCHAR(6) NOT NULL PRIMARY KEY,
+Nama_MK VARCHAR(50) NOT NULL,
+Sks INT NOT NULL
+);
+
+INSERT INTO matakuliah(Kode_MK, Nama_MK, Sks)
+VALUES
+('S01', 'Matematika', '4'),
+('S02', 'Sosiologi', '3'),
+('S03', 'Biopsikologi', '3'),
+('S04', 'Akuntansi Keuangan', '3'),
+('S05', 'Sastra Inggris', '2'),
+('S06', 'Sejarah', '4'),
+('S07', 'Akuntansi Pengantar', '3'),
+('S08', 'Manajemen Keuangan', '2'),
+('S09', 'Algoritma dan Pemrograman', '4'),
+('S010', 'Komputer Grafis', '2');
+
+ 
+EXEC selectMatakuliah;
+
+
+
